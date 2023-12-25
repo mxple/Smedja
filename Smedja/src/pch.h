505f7edd20aabfb55b64c59a2b617554da9e2fb7
RIN <83033020+Mxple@users.noreply.github.com>
2023-12-25 16:02:16 -0500

    Create pch.h

---
+++ Smedja/src/pch.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+#include <algorithm>
+#include <functional>
+#include <memory>
+#include <thread>
+
+#include <chrono>
+
+#include "Smedja/Log.h"
+#include "Smedja/Core.h"
